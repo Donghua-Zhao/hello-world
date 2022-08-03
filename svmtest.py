0ecb1fd3772c03cb2abd914cfc61156168d822d2
Donghua-Zhao <81745334+Donghua-Zhao@users.noreply.github.com>
2022-08-03 18:35:08 +0800

    example

--- svmtest.py
+++ svmtest.py
@@ -1,10 +1,37 @@
-#Import Library
+import matplotlib.pyplot as plt
 from sklearn import svm
-#Assumed you have, X (predictor) and Y (target) for training data set and x_test(predictor) of test_dataset
-# Create SVM classification object 
-model = svm.svc(kernel='linear', c=1, gamma=1) 
-# there is various option associated with it, like changing kernel, gamma and C value. Will discuss more # about it in next section.Train the model using the training sets and check score
-model.fit(X, y)
-model.score(X, y)
-#Predict Output
-predicted= model.predict(x_test)
+from sklearn.datasets import make_blobs
+from sklearn.inspection import DecisionBoundaryDisplay
+
+
+# we create 40 separable points
+X, y = make_blobs(n_samples=40, centers=2, random_state=6)
+
+# fit the model, don't regularize for illustration purposes
+clf = svm.SVC(kernel="linear", C=1000)
+clf.fit(X, y)
+
+plt.scatter(X[:, 0], X[:, 1], c=y, s=30, cmap=plt.cm.Paired)
+
+# plot the decision function
+ax = plt.gca()
+DecisionBoundaryDisplay.from_estimator(
+    clf,
+    X,
+    plot_method="contour",
+    colors="k",
+    levels=[-1, 0, 1],
+    alpha=0.5,
+    linestyles=["--", "-", "--"],
+    ax=ax,
+)
+# plot support vectors
+ax.scatter(
+    clf.support_vectors_[:, 0],
+    clf.support_vectors_[:, 1],
+    s=100,
+    linewidth=1,
+    facecolors="none",
+    edgecolors="k",
+)
+plt.show()
